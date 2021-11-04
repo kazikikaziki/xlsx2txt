08ff24e427c8b9db49d28c090bb3757de8f22a0f
kazikikaziki <doveilbagno@gmail.com>
2021-11-04 17:06:42 +0900

    debug log

--- Game/GameMain.cpp
+++ Game/GameMain.cpp
@@ -7,13 +7,13 @@ using namespace Kamilo;
 bool ExportTextFromXLSX(const std::string &inpath, const std::string &outpath) {
 	KInputStream input = KInputStream::fromFileName(inpath);
 	if (!input.isOpen()) {
-		KLog::printError("Failed to open: %s", inpath.c_str());
+		KLogger::get()->emitf(KLogLv_ERROR, "Failed to open: %s", inpath.c_str());
 		return false;
 	}
 	KExcelFile ef;
 	ef.loadFromStream(input, inpath);
 	if (ef.empty()) {
-		KLog::printError("Invalid excel file: %s", inpath.c_str());
+		KLogger::get()->emitf(KLogLv_ERROR, "Invalid excel file: %s", inpath.c_str());
 		return false;
 	}
 
@@ -22,11 +22,11 @@ bool ExportTextFromXLSX(const std::string &inpath, const std::string &outpath) {
 	// 書き出す
 	KOutputStream output = KOutputStream::fromFileName(outpath);
 	if (!output.isOpen()) {
-		KLog::printError("Failed to open output: %s", outpath.c_str());
+		KLogger::get()->emitf(KLogLv_ERROR, "Failed to open output: %s", outpath.c_str());
 		return false;
 	}
 	output.write(text_u8.data(), text_u8.size());
-	KLog::printText("Output: %s\n", outpath.c_str());
+	KLogger::get()->emitf(KLogLv_NONE, "Output: %s\n", outpath.c_str());
 	return true;
 }
 
@@ -50,24 +50,25 @@ void GameMain(const char *args_ansi) {
 
 	K::sysSetCurrentDir(K::sysGetCurrentExecDir()); // exe の場所をカレントディレクトリにする
 	K::win32_AllocConsole();
-	KLog::setOutputConsole(true);
-	KLog::printText("***");
-	KLog::printText("*** XLSX2TXT (%s) ***", __DATE__);
-	KLog::printText("***");
-	KLog::printText("");
-	KLog::printDebug("ARGS=%s\n", args_u8.c_str());
+	KLogger::open(KLogEmitFlag_INIT_MUTE);
+	KLogger::get()->getEmitter()->setConsoleOutput(true);
+	KLogger::get()->emitf(KLogLv_NONE, "***");
+	KLogger::get()->emitf(KLogLv_NONE, "*** XLSX2TXT (%s) ***", __DATE__);
+	KLogger::get()->emitf(KLogLv_NONE, "***");
+	KLogger::get()->emitf(KLogLv_NONE, "");
+	KLogger::get()->emitf(KLogLv_NONE, "ARGS=%s\n", args_u8.c_str());
 
 	auto tok = K::strSplit(args_u8, " ");
 	for (int i=0; i<tok.size(); i++) {
 		const std::string &in = _Unquote(tok[i]);
 		if (K::pathHasExtension(in, ".xlsx")) {
-			KLog::printText("[%d] %s\n", i, tok[i].c_str());
+			KLogger::get()->emitf(KLogLv_NONE, "[%d] %s\n", i, tok[i].c_str());
 			std::string out = in + ".xlsx2txt";
 			ExportTextFromXLSX(in, out);
 		}
 	}
 
-	printf("[Hit enter key]\n");
-	getchar();
+//	printf("[Hit enter key]\n");
+//	getchar();
 	K::win32_FreeConsole();
 }
